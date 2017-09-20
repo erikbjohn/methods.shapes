415f926372f382fa729ff7b769221947c1bab235
erikbjohn <erikbjohn@gmail.com>
2017-09-20 11:07:51 -0500

    Included line length

--- R/methods.shapes.R
+++ R/methods.shapes.R
@@ -130,6 +130,7 @@ shp.2.lines <- function(shp, proj.env.name=NULL){
     df <- data.frame(ID = row.names(SL1))
     SL1 <- SpatialLinesDataFrame(SL1, df)
     SL1@data$LineLength <- sapply(SL1@lines, function(x) LineLength(Line(slot(slot(x, 'Lines')[[1]], 'coords'))))
+    SL1 <- SL1[SL1@data$LineLength>0,]
     return(SL1)
 }
 #' @title shapes.check.identical
